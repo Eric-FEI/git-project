ecd2b4a88113feba7321edabc42a893b375eed11
eric <eric@gmail.com>
2021-11-23 17:25:24 +0800

    second commit

--- src/main/java/com/eric/GitTest.java
+++ src/main/java/com/eric/GitTest.java
@@ -3,5 +3,6 @@ package com.eric;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, git!");
+        System.out.println("hello, again!");
     }
 }
